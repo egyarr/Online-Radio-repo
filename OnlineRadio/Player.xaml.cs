542a75d25f8ea78611ff896d4d5549720675c2a5
egyarr <egyarr@gmail.com>
2018-06-13 18:17:04 +0300

    ВСЕ ИЗМЕНЕНИЯ СДЕЛАЛ ДАНИЛА ШЕПЕЛЕВ! 1) Добавлена первая радиоволна! 2) Библиотека WMPLib.dll

--- OnlineRadio/Player.xaml.cs
+++ OnlineRadio/Player.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WMPLib;
 
 namespace OnlineRadio
 {
@@ -19,25 +20,34 @@ namespace OnlineRadio
     /// </summary>
     public partial class Player : Window
     {
+
+        WindowsMediaPlayer wmPlayer = new WindowsMediaPlayer();
+        string URL;
+
         public Player()
         {
             InitializeComponent();
+            URL = @"http://nashe1.hostingradio.ru/rock-128.mp3";
         }
 
         System.Media.SoundPlayer player = new System.Media.SoundPlayer();
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            wmPlayer.controls.pause();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            wmPlayer.URL = URL;
+            wmPlayer.controls.pause();
+            wmPlayer.controls.play();
             player.Play();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            wmPlayer.controls.stop();
             player.Stop();
         }
     }
