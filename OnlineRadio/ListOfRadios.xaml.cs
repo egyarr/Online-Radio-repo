dd24d5b6065a805bbee8e1e115a543755d843cc2
egyarr <egyarr@gmail.com>
2018-06-13 20:27:15 +0300

    1) Было решено избавиться от окна PLAYER 2) Изменён интерфейс окна, где расположен список с радиостанциями 3) Добавлены 5 станций 4) Специальная панель управления звуком+анимация

--- OnlineRadio/ListOfRadios.xaml.cs
+++ OnlineRadio/ListOfRadios.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WMPLib;
+using WpfAnimatedGif;
 
 namespace OnlineRadio
 {
@@ -19,18 +21,30 @@ namespace OnlineRadio
     /// </summary>
     public partial class ListOfRadios : Window
     {
+
+        WindowsMediaPlayer wmPlayer = new WindowsMediaPlayer();
+        string ROCKurl, CHOCOurl, EUROurl, RETROurl, DORurl;
+        string n = "Now playing: ";
+        
+
         public ListOfRadios()
         {
             InitializeComponent();
+            ROCKurl = @"http://nashe1.hostingradio.ru/rock-128.mp3";
+            CHOCOurl = @"http://choco.hostingradio.ru:10010/fm";
+            EUROurl = @"http://ep256.hostingradio.ru:8052/europaplus256.mp3";
+            RETROurl = @"http://retroserver.streamr.ru:8043/retro256.mp3";
+            DORurl = @"http://dorognoe.hostingradio.ru:8000/dorognoe";
         }
          
         Player pl;
-        
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            pl = new Player();
-            pl.Show();
-        }
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    pl = new Player();
+        //    pl.Show();
+        //}
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
@@ -51,5 +65,61 @@ namespace OnlineRadio
         {
             MessageBoxResult result = MessageBox.Show("Here will be some text");
         }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            wmPlayer.URL = ROCKurl;
+            wmPlayer.controls.play();
+            player.Play();
+            Now_playing.Content = n + "Rock Radio";
+            img.Visibility = Visibility.Visible;
+        }
+
+        private void Stop_Button(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            Now_playing.Content = "Nothing is playing now";
+            img.Visibility = Visibility.Hidden;
+        }
+
+        private void Chocolate(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            wmPlayer.URL = CHOCOurl;
+            wmPlayer.controls.play();
+            player.Play();
+            Now_playing.Content = n+"Radio Chocolate";
+            img.Visibility = Visibility.Visible;
+        }
+        private void Europa_Plus(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            wmPlayer.URL = EUROurl;
+            wmPlayer.controls.play();
+            player.Play();
+            Now_playing.Content = n + "Europa Plus";
+            img.Visibility = Visibility.Visible;
+        }
+        private void Retro(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            wmPlayer.URL = RETROurl;
+            wmPlayer.controls.play();
+            player.Play();
+            Now_playing.Content = n + "Retro FM";
+            img.Visibility = Visibility.Visible;
+        }
+        private void Doroznoe(object sender, RoutedEventArgs e)
+        {
+            wmPlayer.controls.stop();
+            wmPlayer.URL = DORurl;
+            wmPlayer.controls.play();
+            player.Play();
+            Now_playing.Content = n + "Doroznoe Radio";
+            img.Visibility = Visibility.Visible;
+        }
+
+
     }
 }
